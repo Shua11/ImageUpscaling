ca7ec1ca3ca33794c314bb15499a5828f822c101
Mega-Tom <thomas@peckfam.com>
2019-03-29 14:32:02 -0600

    Network fixes

--- network.py
+++ network.py
@@ -16,8 +16,9 @@ datagen = ImageDataGenerator(
         horizontal_flip=True,
         fill_mode='nearest')
 
-x_train = [img_to_array(load_img('../HD pics/DIV2K_train_HR/{:04d}.png'.format(i))) for i in range(1,100)]
-
+images = [load_img('../HD pics/DIV2K_train_HR/{:04d}.png'.format(i+1)) for i in range(100)]
+y_train = np.array([img_to_array(x)[:768, :768, :]/255 for x in images])
+x_train = np.array([img_to_array(x.resize((384,384)))[:384, :384, :]/255 for x in images])
 
 ######################
 input_img = layers.Input(shape=(None, None, 3))
@@ -32,7 +33,7 @@ autoencoder.compile(optimizer='adadelta', loss='mean_absolute_error')
 
 ######################
 
-#autoencoder.fit(x_train, x_train, epochs=1, batch_size=500, shuffle=True)
+autoencoder.fit(x_train, y_train, epochs=1, batch_size=5, shuffle=True)
 
 #decoded_imgs = autoencoder.predict(x_test)
 
