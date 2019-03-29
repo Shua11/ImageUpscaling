d7985e2185190b286a1c9990393472fe38c3c70a
Mega-Tom <thomas@peckfam.com>
2019-03-29 15:52:58 -0600

    Save output images

--- network.py
+++ network.py
@@ -5,9 +5,11 @@ Created on Wed Mar 27 11:23:44 2019
 
 @author: thomas
 """
+import os
 import numpy as np
+import datetime 
 from keras import layers, models
-from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
 
 
 datagen = ImageDataGenerator(
@@ -35,7 +37,15 @@ upscaler.compile(optimizer='adadelta', loss='mean_absolute_error')
 
 ######################
 
-for i in range(len(x_train)):
-    upscaler.fit(np.array([x_train[i]]), np.array([y_train[i]]), epochs=1, batch_size=5, shuffle=True)
-
-#upscaled_imgs = upscaler.predict(x_train)
+for epoch in range(1):
+    print("epoch #{}".format(epoch))
+    for i in range(len(x_train)):
+        upscaler.fit(np.array([x_train[i]]), np.array([y_train[i]]),
+                     epochs=1, batch_size=5, shuffle=True,
+                     verbose=0)
+ 
+outdir = "../output_{}".format(datetime.datetime.now().strftime("%y%m%d"))
+os.mkdir(outdir)
+        
+for i,x in enumerate(x_train):
+    array_to_img(upscaler.predict(np.array([x]))[0]).save("{}/{:04d}.png".format(outdir,i+1))
